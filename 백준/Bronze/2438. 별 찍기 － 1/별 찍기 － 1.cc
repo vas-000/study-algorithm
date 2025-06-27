c45453d145767630dcef24f5350dc362845d30c8
vas-000 <48551557+vas-000@users.noreply.github.com>
2025-06-27 22:36:38 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+class CDraw
+{
+private:
+    int m_rowCount;
+    void draw(int count, char ch){
+        for(int i=0;i<count; ++i){
+            cout<<ch;
+        }
+    }
+    void drawRow(int row){
+        draw(row, '*');
+        cout << endl;
+    }
+public:
+    void Input(){
+        cin >> m_rowCount;
+    }
+    void Print(){
+        for(int i=1; i<= m_rowCount; i++){
+            drawRow(i);
+        }
+    }
+};
+
+int main()
+{
+    CDraw draw;
+    draw.Input();
+    draw.Print();
+    return 0;
+}
