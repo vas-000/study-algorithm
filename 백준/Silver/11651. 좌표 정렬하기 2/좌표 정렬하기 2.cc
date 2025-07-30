502e6301130d58a5247ea959ceeea5624c5cf234
vas-000 <48551557+vas-000@users.noreply.github.com>
2025-07-31 07:10:36 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 104 ms, Memory: 3572 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    vector<pair<int, int>> m_data;
+    int nTestCase;
+    cin>>nTestCase;
+    
+    int x, y;
+    while(nTestCase>0){
+        cin >> x>>y;
+        pair<int, int> data={y,x};
+        m_data.push_back(data);
+        nTestCase--;
+    }
+    sort(m_data.begin(), m_data.end());
+    
+    for(int i=0; i< m_data.size(); ++i){
+        printf("%d %d\n", m_data[i].second, m_data[i].first);
+    }
+    
+    return 0;
+}
