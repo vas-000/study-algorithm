6ef8e4dc9f4138a07587ce2d14fee46ddfebc724
vas-000 <48551557+vas-000@users.noreply.github.com>
2025-09-11 07:24:05 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,111 @@
+#include <cctype>
+#include <cstdio>
+#include <chrono>
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <list>
+#include <cmath>
+#include <algorithm>
+#include <cstring>
+#include <set>
+#include <unordered_set>
+#include <random>
+#include <type_traits>
+#include <utility>
+
+using namespace std;
+using namespace std::chrono;
+
+typedef long long _INT64;
+typedef unsigned long long _UINT64;
+
+namespace UTIL{
+    template<typename T>
+    bool RangeCheck(T& val, T min, T max){
+        if constexpr (std::is_same_v<T, string>){
+            int nMin = stoi(min);
+            int nMax = stoi(max);
+            int nlength = val.length();
+            if( (nlength<nMin)||(nlength>nMax)){
+                return false;
+            }
+        }
+        else{
+            if( (val<min)||(val>max)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool IsAlpha(const std::string& str){
+        for (char ch : str) {
+            if (!std::isalpha(static_cast<unsigned char>(ch))) {
+                return false;
+            }
+        }
+        return true;
+    }  
+
+    template<typename T>
+    bool Input(T& val, T min, T max){
+        cin>>val;
+        return RangeCheck(val, min, max);
+    }
+    std::chrono::high_resolution_clock::time_point g_startTime, g_endTime;
+
+    void StartTimer(){
+        g_startTime = high_resolution_clock::now();
+    }
+    void EndTimer(){
+        g_endTime = high_resolution_clock::now();
+        auto time_span = std::chrono::duration_cast<milliseconds>(g_endTime-g_startTime);
+        cout<< "Elapsed: "<< time_span.count()<<"ms"<<endl;
+    }
+}
+
+class CQuest{
+public:
+    bool InputData(){
+        if(!UTIL::Input(m_height, 1, 5)){
+            return false;
+        }
+
+        return true;
+    }
+    void Process(){
+        // 1층 2개 2N
+        // 2층 4개
+        // 3층 8개
+        m_total = pow(2, m_height);
+
+    }
+    void Print(){
+        cout<<m_total<<'\n';
+    }
+private:
+    int m_total;
+    int m_height; // 탑의 높이
+    
+   
+};
+
+int main()
+{
+    //UTIL::StartTimer();
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    CQuest quest;
+    if (!quest.InputData()){
+        return -1;
+    } 
+    quest.Process(); 
+    quest.Print();
+    //UTIL::EndTimer();
+    
+    return 0;
+}
