9838a3e0734201e352fea9e543f7654684088852
vas-000 <48551557+vas-000@users.noreply.github.com>
2025-09-15 07:22:53 +0900

    [Bronze I] Title: 약수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.cc
@@ -0,0 +1,119 @@
+#include <cctype>
+#include <cstdio>
+#include <chrono>
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <list>
+#include <cmath>
+#include <algorithm>
+#include <cstring>
+#include <set>
+#include <unordered_set>
+#include <random>
+#include <type_traits>
+#include <utility>
+
+using namespace std;
+using namespace std::chrono;
+
+typedef long long _INT64;
+typedef unsigned long long _UINT64;
+
+namespace UTIL{
+    template<typename T>
+    bool RangeCheck(T& val, T min, T max){
+        if constexpr (std::is_same_v<T, string>){
+            int nMin = stoi(min);
+            int nMax = stoi(max);
+            int nlength = val.length();
+            if( (nlength<nMin)||(nlength>nMax)){
+                return false;
+            }
+        }
+        else{
+            if( (val<min)||(val>max)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool IsAlpha(const std::string& str){
+        for (char ch : str) {
+            if (!std::isalpha(static_cast<unsigned char>(ch))) {
+                return false;
+            }
+        }
+        return true;
+    }  
+
+    template<typename T>
+    bool Input(T& val, T min, T max){
+        cin>>val;
+        return RangeCheck(val, min, max);
+    }
+    std::chrono::high_resolution_clock::time_point g_startTime, g_endTime;
+
+    void StartTimer(){
+        g_startTime = high_resolution_clock::now();
+    }
+    void EndTimer(){
+        g_endTime = high_resolution_clock::now();
+        auto time_span = std::chrono::duration_cast<milliseconds>(g_endTime-g_startTime);
+        cout<< "Elapsed: "<< time_span.count()<<"ms"<<endl;
+    }
+}
+
+class CQuest{
+public:
+    bool InputData(){
+        int count; //  약수의 갯수
+        if(!UTIL::Input(count, 0, 50)){
+            return false;
+        }
+        for(int i{}; i<count; ++i){
+            int data;
+            if(!UTIL::Input(data, 1, 1'000'000)){
+                return false;
+            }
+            m_divisor.push_back(data);
+        }
+
+        return true;
+    }
+    void Process(){
+        if( m_divisor.size()==1){
+            m_ret = m_divisor.at(0)* m_divisor.at(0);
+            return;
+        }
+        sort(m_divisor.begin(), m_divisor.end());
+        m_ret = m_divisor.at(0) * m_divisor.at(m_divisor.size()-1);
+
+    }
+    void Print(){
+        cout<<m_ret<<'\n';
+    }
+private:
+    vector<int> m_divisor;
+    int m_ret;
+};
+
+int main()
+{
+    //UTIL::StartTimer();
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    CQuest quest;
+    if (!quest.InputData()){
+        return -1;
+    } 
+    quest.Process(); 
+    quest.Print();
+    //UTIL::EndTimer();
+    
+    return 0;
+}
