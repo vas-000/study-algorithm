635692ee77351b0acca7196c2c1fbb6f9bcac312
vas-000 <48551557+vas-000@users.noreply.github.com>
2025-09-16 07:24:27 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 92 ms, Memory: 16948 KB -BaekjoonHub

---
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.cc
@@ -0,0 +1,125 @@
+#include <cctype>
+#include <cstdio>
+#include <chrono>
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <list>
+#include <cmath>
+#include <algorithm>
+#include <cstring>
+#include <set>
+#include <unordered_set>
+#include <random>
+#include <type_traits>
+#include <utility>
+
+using namespace std;
+using namespace std::chrono;
+
+typedef long long _INT64;
+typedef unsigned long long _UINT64;
+
+namespace UTIL{
+    template<typename T>
+    bool RangeCheck(T& val, T min, T max){
+        if constexpr (std::is_same_v<T, string>){
+            int nMin = stoi(min);
+            int nMax = stoi(max);
+            int nlength = val.length();
+            if( (nlength<nMin)||(nlength>nMax)){
+                return false;
+            }
+        }
+        else{
+            if( (val<min)||(val>max)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool IsAlpha(const std::string& str){
+        for (char ch : str) {
+            if (!std::isalpha(static_cast<unsigned char>(ch))) {
+                return false;
+            }
+        }
+        return true;
+    }  
+
+    template<typename T>
+    bool Input(T& val, T min, T max){
+        cin>>val;
+        return RangeCheck(val, min, max);
+    }
+    std::chrono::high_resolution_clock::time_point g_startTime, g_endTime;
+
+    void StartTimer(){
+        g_startTime = high_resolution_clock::now();
+    }
+    void EndTimer(){
+        g_endTime = high_resolution_clock::now();
+        auto time_span = std::chrono::duration_cast<milliseconds>(g_endTime-g_startTime);
+        cout<< "Elapsed: "<< time_span.count()<<"ms"<<endl;
+    }
+}
+
+class CQuest{
+public:
+    bool InputData(){
+        int count; //채팅 갯수
+        if(!UTIL::Input(count, 0, 100'000)){
+            return false;
+        }
+        set<string> data;
+        for(int i{}; i<count; ++i){
+            string name;
+            if(!UTIL::Input<string>(name, "1", "20")){
+                return false;
+            }
+            if(name.compare("ENTER")==0){
+                if(!name.empty())
+                    m_data.push_back(data);
+                data.clear();
+                continue;
+            }
+            data.insert(name);
+        }
+        if(!data.empty()){
+            m_data.push_back(data);
+        }
+
+        return true;
+    }
+    void Process(){
+        for(auto data:m_data){
+            m_ret += data.size();
+        }
+    }
+    void Print(){
+        cout<<m_ret<<'\n';
+    }
+private:
+    list<set<string>> m_data;
+    int m_ret=0;
+};
+
+int main()
+{
+    //UTIL::StartTimer();
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    CQuest quest;
+    if (!quest.InputData()){
+        return -1;
+    } 
+    quest.Process(); 
+    quest.Print();
+    //UTIL::EndTimer();
+    
+    return 0;
+}
