ece04d2e0ad6745bed62b7e79fdb1bddb558667f
vas-000 <48551557+vas-000@users.noreply.github.com>
2025-10-29 08:39:15 +0900

    [Silver V] Title: 올림픽, Time: 0 ms, Memory: 2156 KB, Score: 45 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.cc
+++ 백준/Silver/8979. 올림픽/올림픽.cc
@@ -1,48 +1,213 @@
+#include <cctype>
+#include <cstdio>
+#include <chrono>
 #include <iostream>
+#include <iterator>
+#include <sstream>
 #include <vector>
+#include <map>
+#include <list>
+#include <cmath>
+#include <algorithm>
+#include <cstring>
+#include <set>
+#include <unordered_set>
+#include <random>
+#include <type_traits>
+#include <utility>
+
 using namespace std;
-int main() 
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
-    int N;
-    cin >> N;
-    
-    if (N < 1 || N > 1000) return 0;
-    
-    int find_country;
-    cin >> find_country;
-    
-    vector<int> gold(N+1);
-    vector<int> silver(N+1);
-    vector<int> bronze(N+1);
-    
-    int country_num = 0;
-    
-    for(int i = 0; i < N; i++)
-    {
-        cin >> country_num;
-        cin >> gold[country_num];
-        cin >> silver[country_num];
-        cin >> bronze[country_num];   
+using namespace std::chrono;
+
+typedef long long _INT64;
+typedef unsigned long long _UINT64;
+
+namespace UTIL{
+    template<typename T>
+    bool RangeCheck(T& val, T min, T max){
+        if constexpr (std::is_same_v<T, string>){
+            int nMin = stoi(min);
+            int nMax = stoi(max);
+            int nlength = val.length();
+            if( (nlength<nMin)||(nlength>nMax)){
+                return false;
+            }
+        }
+        else{
+            if( (val<min)||(val>max)){
+                return false;
+            }
+        }
+        return true;
     }
-    
-    int rank = 1;
-    for (int i = 1; i <= N; ++i) 
-    {
-        if (i == find_country) 
-            continue;
+
+    bool IsAlpha(const std::string& str){
+        for (char ch : str) {
+            if (!std::isalpha(static_cast<unsigned char>(ch))) {
+                return false;
+            }
+        }
+        return true;
+    }  
+
+    template<typename T>
+    bool Input(T& val, T min, T max){
+        cin>>val;
+        return RangeCheck(val, min, max);
+    }
+    std::chrono::high_resolution_clock::time_point g_startTime, g_endTime;
+
+    void StartTimer(){
+        g_startTime = high_resolution_clock::now();
+    }
+    void EndTimer(){
+        g_endTime = high_resolution_clock::now();
+        auto time_span = std::chrono::duration_cast<milliseconds>(g_endTime-g_startTime);
+        cout<< "Elapsed: "<< time_span.count()<<"ms"<<endl;
+    }
+}
+
+typedef struct ST_MEDAL{
+    int gold;
+    int silver;
+    int bronze;
+    bool isSame(ST_MEDAL other){
+        if( gold != other.gold)
+            return false;
+        if( silver != other.silver)
+            return false;
+        if( bronze != other.bronze)
+            return false;
+        return true;
+    }
+}stMedal;
+
+class CNation{
+private:
+    int code;
+    stMedal m_medal;
+    int ranking;
+public:
+    CNation(int aC, int aG, int aS, int aB)
+    :code(aC){
+        m_medal.gold=aG;
+        m_medal.silver=aS;
+        m_medal.bronze=aB;
+    }
+    stMedal GetMedal() {return m_medal;}
+    int GetCode(){return code;}
+    int GetRank(){return ranking;}
+    void SetRanking(int rank){
+        ranking = rank;
+    }
+
+};
+class CQuest{
+public:
+    bool InputData(){
         
-        if (gold[i] > gold[find_country] ||
-            (gold[i] == gold[find_country] && silver[i] > silver[find_country]) ||
-            (gold[i] == gold[find_country] && silver[i] == silver[find_country] 
-             && bronze[i] > bronze[find_country])) 
-        {
-            rank++;
+        if(!UTIL::Input(m_count, 1, 1'000))
+            return false;
+
+        if(!UTIL::Input(m_nation, 1, m_count))
+            return false;
+
+        int totalMedal=0;
+        for(int i{}; i< m_count; ++i){
+            //전체 메달의 수는 1'000'000 이하 이다.
+            int code, gold, silver, bronze;
+            if(!UTIL::Input(code, 1, 1'000)){
+                return false;
+            }
+
+            if(!UTIL::Input(gold, 0, 1'000'000)){
+                return false;
+            }
+
+            if(!UTIL::Input(silver, 0, 1'000'000)){
+                return false;
+            }
+
+            if(!UTIL::Input(bronze, 0, 1'000'000)){
+                return false;
+            }
+
+    
+            totalMedal = totalMedal+gold+silver+bronze;
+            if( totalMedal > 1'000'000){
+                return false;
+            }
+
+            CNation nation(code, gold, silver, bronze);
+            m_data.push_back(nation);
+        }
+    
+        return true;
+    }
+    void Process(){
+        // 금, 은, 동 순으로 나라를 정렬한다. 
+        sort(m_data.begin(), m_data.end(), []( CNation& first, CNation& second){
+           stMedal nation1 = first.GetMedal();
+           stMedal nation2 = second.GetMedal();
+           if( nation1.gold < nation2.gold){
+                return false;
+           }
+           else if( nation1.gold == nation2.gold){
+                if(nation1.silver < nation2.silver){
+                    return false;
+                }
+                else if( nation1.silver == nation2.silver){
+                    if( nation1.bronze < nation2. bronze){
+                        return false;
+                    }
+                }
+           }
+           return true;
+        });
+
+        //정렬된 나라순으로 Ranking을 계산한다. 
+        int ranking=1;
+        int inc=0;
+        stMedal pre = m_data.begin()->GetMedal();
+        for(auto &nation: m_data){
+            stMedal cur = nation.GetMedal();
+            if(!pre.isSame(cur)){
+                ranking=ranking+inc;
+                inc=0;
+                pre=cur;    
+            }
+            nation.SetRanking(ranking);
+            inc++;
+        }
+
+    } 
+    void Print(){
+        for(auto nation: m_data){
+            if( nation.GetCode()==m_nation){
+                cout<< nation.GetRank()<<"\n";
+                return;
+            }
         }
     }
+private:
+    int m_count=0;
+    int m_nation=0;
+    vector<CNation> m_data;
+};
+
+int main()
+{
+    //UTIL::StartTimer();
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    CQuest quest;
+    if (!quest.InputData()){
+        return -1;
+    } 
+    quest.Process(); 
+    quest.Print();
+    //UTIL::EndTimer();
     
-    cout << rank << '\n';
-    
+    return 0;
 }
